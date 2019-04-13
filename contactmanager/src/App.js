eb403a0e09c54905ae8a34892d00484c54b19788
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2019-04-12 22:34:13 -0600

    added action and reducer

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import Contact from './components/Contact';
+import Contacts from './components/Contacts';
 import Header from './components/Header';
+
+import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -8,19 +10,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <Provider>
       <div className="App">
         <Header branding="Contact Manager" />
-       <Contact 
-       name="John Doe"
-       email="jdoe@gmail.com"
-       phone="555-555-5555"
-       />
-       <Contact 
-       name="Karen Smith"
-       email="karen@gmail.com"
-       phone="555-555-5554"
-       />
+        <div className="container">
+        <Contacts />
+        </div>
+      
       </div>
+      </Provider>
     );
   }
 }
