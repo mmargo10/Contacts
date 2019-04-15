24f1827fb9b7d87fd944d1c9bc2e3bbf59a2dff0
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2019-04-15 11:28:18 -0600

    added test component

---
+++ contactmanager/src/components/test/Test.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+  state = {
+    test: 'test'
+  };
+
+componentDidMount() {
+  console.log('componentDidMount...');
+}
+
+//componentWillMount() {
+//console.log('componentWillMount...');
+//}
+
+//componentDidUpdate() {
+  //console.log('componentDidUpdate...');
+//}
+
+//componentWillUpdate() {
+  //console.log('componentWillUpdate...');
+//}
+
+//componentWillReceiveProps(nextProps, nextState) {
+  //console.log('componentWillReceiveProps...');
+//}
+
+//static getDerivedStateFromProps(nextProps, prevState) {
+  //return null;
+//}
+
+//getSnapshotBeforeUpdate(prevProps, prevState) {
+  //console.log('getSnapshotBeforeUpdate...');
+//}
+
+  render() {
+    return (
+      <div>
+       <h1>Test Component</h1> 
+      </div>
+    )
+  }
+}
+
+export default Test;
